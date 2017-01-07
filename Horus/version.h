3bffd0eaddaf25da511b82da6d2624ed59071bf0
Daniel Caujolle-Bert <danielvador@gmail.com>
2017-01-07 11:50:02 +0100

    Prepare for release. Bump version.

--- Horus/version.h
+++ Horus/version.h
@@ -4,26 +4,26 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "06";
+	static const char DATE[] = "07";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2017";
 	static const char UBUNTU_VERSION_STYLE[] =  "17.01";
 	
 	//Software Status
-	static const char STATUS[] =  "Beta";
-	static const char STATUS_SHORT[] =  "b";
+	static const char STATUS[] =  "Release";
+	static const char STATUS_SHORT[] =  "r";
 	
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 99;
-	static const long BUILD  = 8;
+	static const long BUILD  = 10;
 	static const long REVISION  = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 61;
-	#define RC_FILEVERSION 0,99,8,0
-	#define RC_FILEVERSION_STRING "0, 99, 8, 0\0"
-	static const char FULLVERSION_STRING [] = "0.99.8.0";
+	static const long BUILDS_COUNT  = 246;
+	#define RC_FILEVERSION 0,99,10,0
+	#define RC_FILEVERSION_STRING "0, 99, 10, 0\0"
+	static const char FULLVERSION_STRING [] = "0.99.10.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 1;
